657b3d8359528758d6828c00a4b610f2ed9e791a
EC2 Default User <ec2-user@ip-172-31-64-111.ec2.internal>
2021-10-30 22:12:07 +0000

    subject

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4292eb477c37f23e79791246b42394712f3c6cae15823efa92f95949c273225cd203da8bc48443c13d7436e415d29bbf47b404751954b91b30eafa5538f47d06'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
+  
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+  
+end
